d663905a0cc6cc1093a558086c31f302cb158c14
sam <ruwesam@gmail.com>
2023-02-02 11:38:16 -0500

    Add fatJar task to financial-definition-helper

--- financial-definition-helper/build.gradle.kts
+++ financial-definition-helper/build.gradle.kts
@@ -6,3 +6,20 @@ plugins {
 application {
     mainClass.set("cli.app.AppKt")
 }
+
+tasks.register<Jar>("fatJar") {
+    dependsOn.addAll(
+        listOf(
+            "compileJava",
+            "compileKotlin",
+            "processResources"
+        )
+    ) // We need this for Gradle optimization to work
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+        .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+    from(contents)
+}
